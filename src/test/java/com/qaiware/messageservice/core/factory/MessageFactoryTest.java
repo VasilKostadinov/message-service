34b4e5cdf8afc8d34bde9d712d3b74bee54ee02b
Vasil.Kostadinov <vasil.dm.kostadinov@gmail.com>
2018-08-25 09:52:06 +0300

    Added invalid type test for MessageFactory and test mappings to test

--- src/test/java/com/qaiware/messageservice/core/factory/MessageFactoryTest.java
+++ src/test/java/com/qaiware/messageservice/core/factory/MessageFactoryTest.java
@@ -3,7 +3,9 @@ package com.qaiware.messageservice.core.factory;
 import com.qaiware.messageservice.core.message.AbstractMessage;
 import com.qaiware.messageservice.core.message.EmotionMessage;
 import com.qaiware.messageservice.core.message.TextMessage;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +13,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class MessageFactoryTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
     private MessageFactory messageFactory = new PropertiesConfigMessageFactory("message-mappings.properties");
 
@@ -56,5 +61,15 @@ public class MessageFactoryTest {
         assertEquals(id,message.getId());
         assertEquals(payload,message.getPayload());
         assertEquals(createdAt,message.getCreatedAt());
+    }
+
+    @Test
+    public void shouldThrowExceptionForInvalidType(){
+        Integer id = 1;
+        String messageType = "INVALID";
+        String payload = "test";
+        LocalDateTime createdAt = LocalDateTime.now();
+        exception.expect(NullPointerException.class);
+        this.messageFactory.createMessage(id,messageType,payload,createdAt);
     }
 }
